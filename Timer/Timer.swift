a353974fa2bdab77dac486c15a32fbe6e1c1d42d
eric mead <ermead@vt.edu>
2015-10-20 16:21:05 -0600

    placehoolders set

--- Timer/Timer.swift
+++ Timer/Timer.swift
@@ -75,8 +75,23 @@ func timerString() -> String{
     let minutes = (totalSeconds/60 - (hours*60))
     let seconds = (totalSeconds - (minutes*60) - (hours*3600))
     
+    var secondsString = ""
+    if seconds < 10 {
+        secondsString = "0\(seconds)"
+    } else { secondsString = "\(seconds)"}
     
-    return "\(hours): \(minutes): \(seconds)"
+    var minutesString = ""
+    if minutes < 10 {
+        minutesString = "0\(minutes)"
+    } else { minutesString = "\(minutes)"}
+    
+    var hoursString = ""
+    if hours > 0 {
+        hoursString = "\(hours)"
+    }
+    
+    return ("\(hoursString):\(minutesString):\(secondsString)")
+    
 
 }
 
