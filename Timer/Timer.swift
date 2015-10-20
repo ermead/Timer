0ced59d0a2220831bebd14a4bf4a8b194510077d
eric mead <ermead@vt.edu>
2015-10-20 08:42:46 -0600

    keep on

--- Timer/Timer.swift
+++ Timer/Timer.swift
@@ -16,26 +16,61 @@ class Timer: NSObject {
     private(set) var seconds = NSTimeInterval(0)
     private(set) var totalSeconds = NSTimeInterval(0)
     private var timer: NSTimer?
-    var isOn: Bool
+    var isOn: Bool{
+        get {
+            if timer == nil {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+
+private var localNotification = UILocalNotification?()
     
-    func setTime(seconds:NSTimeInterval, totalSeconds:NSTimeInterval){
+func setTime(seconds:NSTimeInterval, totalSeconds:NSTimeInterval){
         self.seconds = seconds
         self.totalSeconds = totalSeconds
     }
-}
+
 
 func startTimer(){
-    
+    if (timer == nil) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "secondTick", userInfo: nil, repeats: true)
+        
+        armNotification()
+    }
 }
 
 func stopTimer(){
+    if let timer = timer {
+        timer.invalidate()
+        self.timer = nil
+        if let localNotification = localNotification {
+            UIApplication.sharedApplication().cancelLocalNotification(localNotification)
+        }
+    }
+
     
 }
 
 func secondTick(){
+    if timer != nil {
+        seconds--;
+        NSNotificationCenter.defaultCenter().postNotificationName(Timer.TimerSecondTickNotification, object: self)
+        if seconds <= 0 {
+            NSNotificationCenter.defaultCenter().postNotificationName(Timer.TimerCompleteNotification, object: self)
+            stopTimer()
+        }
+    }
     
 }
 
 func timerString() -> String{
   return ""
+}
+
+func armNotification(){
+        
+}
 }
